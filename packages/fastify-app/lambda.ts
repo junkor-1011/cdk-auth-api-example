8ff1a95516659840913fdb008ad407dedaa1171f
junkor-1011 <tomoyukizxy@gmail.com>
2023-01-24 01:22:50 +0900

    add comment

--- packages/fastify-app/lambda.ts
+++ packages/fastify-app/lambda.ts
@@ -2,8 +2,7 @@ import awsLambdaFastify from '@fastify/aws-lambda';
 import type { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { build } from './src/app';
 
-// eslint-disable-next-line
-// exports.handler = async(event: APIGatewayProxyEvent, context: Context) => proxy(event, context)
+// TODO: generate db client out of Lambda Handler.
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
