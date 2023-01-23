68d2cd55cc7378aefcc747646dbf449ae77d4a67
junkor-1011 <tomoyukizxy@gmail.com>
2023-01-24 00:29:23 +0900

    add workspack: fastify-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['packages/cdk-app/tsconfig.json'],
+    project: ['packages/cdk-app/tsconfig.json', 'packages/fastify-app/tsconfig.json'],
   },
   rules: {},
   overrides: [],
---
+++ packages/fastify-app/src/app.ts
@@ -0,0 +1,27 @@
+import fastify, { FastifyInstance, FastifyServerOptions } from 'fastify';
+import fastifySensible from '@fastify/sensible';
+
+const defaultOption: FastifyServerOptions = {
+  logger: true,
+};
+
+export const build = async (
+  opts: FastifyServerOptions = defaultOption,
+): Promise<FastifyInstance> => {
+  const server = fastify(opts);
+
+  await server.register(fastifySensible);
+
+  return await server;
+};
+
+export const app = async (opts: FastifyServerOptions = defaultOption): Promise<void> => {
+  const server = await build(opts);
+
+  try {
+    console.log('listen port 3000');
+    await server.listen({ port: 3000, host: '0.0.0.0' });
+  } catch (err) {
+    console.log(err);
+  }
+};
