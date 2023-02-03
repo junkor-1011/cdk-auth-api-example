74a37094238cdddae456a798b9b1ea907b2458b1
junkor-1011 <tomoyukizxy@gmail.com>
2023-02-04 02:40:34 +0900

    chore: resolve test

--- packages/lambda/lib/calc.test.ts
+++ packages/lambda/lib/calc.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 
-import { sum } from './calc.js';
+// import { sum } from './calc.js';
+import { sum } from '$lambda/lib/calc.js';
 
 describe('sum', () => {
   it('1 + 1 = 2', () => {
