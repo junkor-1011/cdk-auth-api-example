7ddb3285954c60a2f00649e4fd0dd2372ca000bf
junkor-1011 <tomoyukizxy@gmail.com>
2023-02-07 01:05:42 +0900

    gen client

--- packages/fastify-app/src/lib/prisma-client.ts
+++ packages/fastify-app/src/lib/prisma-client.ts
@@ -1,12 +1,40 @@
+import { z } from 'zod';
 import { type Prisma, PrismaClient } from '@prisma/client';
+import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
 
-const dataSource = {
-  url: process.env.DATABASE_URL,
-} as const satisfies Prisma.Datasource;
+const secretsSchema = z.object({
+  dbClusterIdentifier: z.string(),
+  password: z.string(),
+  engine: z.string().default('postgres'),
+  port: z.number().default(5432),
+  host: z.string(),
+  username: z.string(),
+});
+
+const genDataSource = async (): Promise<Prisma.Datasource> => {
+  if (process.env.STAGE === 'LOCAL') {
+    return {
+      url: process.env.DATABASE_URL,
+    };
+  }
+
+  const client = new SecretsManagerClient({});
+  const command = new GetSecretValueCommand({
+    SecretId: process.env.SECRET_NAME,
+  });
+  const result = await client.send(command);
+  const secrets = secretsSchema.parse(result.SecretString);
+  const { username, password, host, port } = secrets;
+
+  const url = `postgresql://${username}:${password}@${host}:${port}/appdb?schema=public&connection_limit=1`;
+  return { url };
+};
+
+const db = await genDataSource();
 
 export const prisma = new PrismaClient({
   log: ['info', 'warn', 'error', 'query'],
   datasources: {
-    db: dataSource,
+    db,
   },
 });
