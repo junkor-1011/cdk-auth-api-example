16dc463a2e2119731465cf1383a11807c9a78695
junkor-1011 <tomoyukizxy@gmail.com>
2023-02-28 02:15:01 +0900

    mock test

--- packages/lambda/functions/hello/get.test.ts
+++ packages/lambda/functions/hello/get.test.ts
@@ -2,10 +2,16 @@ import { afterEach, describe, expect, it, vi } from 'vitest';
 import { mock } from 'vitest-mock-extended';
 
 import type { APIGatewayProxyEvent } from 'aws-lambda';
-// import axios from 'axios';
+import axios from 'axios';
 import { lambdaHandler } from './get.js';
 
-// vi.mock('axios');
+const dummyIp = '128.66.0.1';
+vi.mock('axios', () => ({
+  // eslint-disable-next-line @typescript-eslint/require-await
+  default: vi.fn(async () => ({
+    data: dummyIp,
+  })),
+}));
 
 afterEach(() => {
   vi.resetAllMocks();
@@ -21,5 +27,9 @@ describe('lambdaHandler', () => {
     expect(response.statusCode).toBe(200);
     const responsePayload = JSON.parse(response.body) as Record<string, unknown>;
     expect(responsePayload?.message).toBe('hello world');
+    console.log(responsePayload);
+    expect(responsePayload?.url).toBe(dummyIp);
+    expect(axios).toHaveBeenCalled();
+    expect(axios).toHaveBeenCalledWith('http://checkip.amazonaws.com');
   });
 });
